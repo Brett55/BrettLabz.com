acdbc583172364f1f97de366358e10c9f6ec1dce
brett55 <bjcull2@gmail.com>
2015-02-16 12:45:30 -0500

    working viewer

--- angularify/app/scripts/app.js
+++ angularify/app/scripts/app.js
@@ -25,21 +25,21 @@ angular
                 templateUrl: 'views/main.html',
                 controller: 'MainCtrl'
             })
-            .state('main.intro', {
+            .state('main.0', {
                 url: 'intro',
                 templateUrl: 'views/main.intro.html'
             })
-            .state('main.geo', {
+            .state('main.1', {
                 url: 'geo',
                 templateUrl: 'views/geo.html'
-            });
-            // .state('main.intro', {
-            //     url: 'intro',
-            //     templateUrl: 'views/main.intro.html'
-            // });
-            // .state('main.intro', {
-            //     url: 'intro',
-            //     templateUrl: 'views/main.intro.html'
-            // });                                    
+            })
+            .state('main.2', {
+                url: 'dev',
+                templateUrl: 'views/dev.html'
+            })
+            .state('main.3', {
+                url: 'projects',
+                templateUrl: 'views/projects.html'
+            });                                            
         $urlRouterProvider.otherwise("/");
     });
--- angularify/app/scripts/controllers/main.js
+++ angularify/app/scripts/controllers/main.js
@@ -8,6 +8,22 @@
  * Controller of the angularifyApp
  */
 angular.module('angularifyApp')
-    .controller('MainCtrl', function($scope, $state) {
-        $state.go('main.intro');
+    .controller('MainCtrl', function($scope, $state, $stateParams) {
+        $state.go('main.0');
+
+        $scope.id = 0;
+
+        $scope.moreInfo = function() {
+        	if ($scope.id < 3) {	
+	        	$scope.id++;
+	        	$state.go("main." + $scope.id);
+	        }
+        };
+
+        $scope.lessInfo = function() {
+        	if ($scope.id > 0) {	
+	        	$scope.id--;
+	        	$state.go("main." + $scope.id);
+	        }
+        };
     });
