528584e3b7e1bbb8b84b1a524d1e899114b66456
brett55 <bjcull2@gmail.com>
2015-02-26 14:06:05 -0500

    added hover effects, refactored into service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,37 +9,37 @@
  * Main module of the application.
  */
 angular
-    .module('angularifyApp', [
-        'ngAnimate',
-        'ngCookies',
-        'ngResource',
-        'ngRoute',
-        'ngSanitize',
-        'ngTouch',
-        'ui.router'
-    ])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('main', {
-                url: '/',
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .state('main.0', {
-                url: 'intro',
-                templateUrl: 'views/main.intro.html'
-            })
-            .state('main.1', {
-                url: 'geo',
-                templateUrl: 'views/geo.html'
-            })
-            .state('main.2', {
-                url: 'dev',
-                templateUrl: 'views/dev.html'
-            })
-            .state('main.3', {
-                url: 'projects',
-                templateUrl: 'views/projects.html'
-            });                                            
-        $urlRouterProvider.otherwise("/");
-    });
+  .module('angularifyApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('main.0', {
+        url: 'intro',
+        templateUrl: 'views/main.intro.html'
+      })
+      .state('main.1', {
+        url: 'geo',
+        templateUrl: 'views/geo.html'
+      })
+      .state('main.2', {
+        url: 'dev',
+        templateUrl: 'views/dev.html'
+      })
+      .state('main.3', {
+        url: 'projects',
+        templateUrl: 'views/projects.html'
+      });
+    $urlRouterProvider.otherwise("/");
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,22 +8,25 @@
  * Controller of the angularifyApp
  */
 angular.module('angularifyApp')
-    .controller('MainCtrl', function($scope, $state, $stateParams) {
-        $state.go('main.0');
-
-        $scope.id = 0;
-
-        $scope.moreInfo = function() {
-        	if ($scope.id < 3) {	
-	        	$scope.id++;
-	        	$state.go("main." + $scope.id);
-	        }
-        };
-
-        $scope.lessInfo = function() {
-        	if ($scope.id > 0) {	
-	        	$scope.id--;
-	        	$state.go("main." + $scope.id);
-	        }
-        };
-    });
+  .controller('MainCtrl', function ($scope, $state, $stateParams, swapInfo) {
+    $state.go('main.0');
+    $scope.id = 0;
+    $scope.moreInfo = swapInfo.moreInfo;
+    $scope.lessInfo = swapInfo.lessInfo;
+  })
+  .service('swapInfo', function($state) {
+    this.moreInfo = function (id) {
+      if (id < 3) {
+        id++;
+      }
+      $state.go("main." + id);
+      return id;
+    };
+    this.lessInfo = function (id) {
+      if (id > 0) {
+        id--;
+      }
+      $state.go("main." + id);
+      return id;
+    };
+  });
